905a7459bf5b6dc624ace8b41647093a2ed59d90
lross2k <luisross2000@outlook.com>
2022-06-11 10:31:20 -0600

    Gaussian Elimination implemented in Python

---
+++ Python/ElimGauss.py
@@ -0,0 +1,30 @@
+# Simple transpose function hack
+def transpose(x):
+    try:
+        len(x[0])
+    except TypeError:
+        for i in range(len(x)): x[i] = [x[i]] 
+    return x
+
+def elim_gauss(A,b):
+    x = []
+    # Constant declarations
+    n = len(A)	# Matrix dimension
+    # Forward elimination process
+    for k in range(n-1):
+        for i in range(k+1,n):
+            F = A[i][k]/A[k][k]
+            for j in range(n):
+                A[i][j] = A[i][j] - F*float(A[k][j])
+            b[i][0] = b[i][0] - F*float(b[k][0])
+    # Reverse substitution process
+    for i in range(0,n): x.append(0) # Populate x
+    for i in range(n-1,-1,-1):
+        suma = 0
+        for j in range(i+1,n):
+            suma = suma + A[i][j]*x[j]
+        x[i] = (b[i][0] - suma)/A[i][i]
+    # Transpose result 
+    #x = x';
+    x = transpose(x)
+    return x
