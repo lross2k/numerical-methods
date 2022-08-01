5486d0ec3082bdc369ee5bf8398f7c14981e859f
lross2k <luisross2000@outlook.com>
2022-07-31 21:43:34 -0600

    Implemented Gaussian Elimination method in JS, with Complex number support
    Essentially, I'm slowly creating a new JavaScript library... Dang it. -lross2k

--- Javascript/ElimGauss.js
+++ Javascript/ElimGauss.js
@@ -4,6 +4,42 @@ class Complex {
         this.a = a;
         this.b = b;
     }
+    cdiv(c2) {
+        let a = this.a;
+        let b = this.b;
+        let c = c2.a;
+        let d = c2.b;
+        return new Complex( (a*c + b*d)/(c*c + d*d), 
+                            (b*c - a*d)/(c*c + d*d)
+                          );
+    }
+    cmul(c2) {
+        let a = this.a;
+        let b = this.b;
+        let c = c2.a;
+        let d = c2.b;
+        return new Complex( (a*c + b*d), 
+                            (a*d + b*c)
+                          );
+    }
+    csub(c2) {
+        let a = this.a;
+        let b = this.b;
+        let c = c2.a;
+        let d = c2.b;
+        return new Complex( a-c,
+                            b-d
+                          );
+    }
+    cadd(c2) {
+        let a = this.a;
+        let b = this.b;
+        let c = c2.a;
+        let d = c2.b;
+        return new Complex( a+c,
+                            b+d
+                          );
+    }
 }
 
 // Simple transpose function hack
@@ -22,7 +58,6 @@ function  elimGauss(A,b) {
     for (let k = 0; k < n-1; k++) {
         for (let i = k+1; i < n; i++) {
             F = A[i][k]/A[k][k];
-            console.log(A[k][k]);
             for (let j = 0; j < n; j++) {
                 A[i][j] = A[i][j] - F*A[k][j];
             }
@@ -44,6 +79,56 @@ function  elimGauss(A,b) {
     return x;
 }
 
+function  elimGaussComplex(A,b) {
+    x = [];
+    // Constant declarations
+    const n = A.length	// Matrix dimension
+    // Forward elimination process
+    for (let k = 0; k < n-1; k++) {
+        for (let i = k+1; i < n; i++) {
+            if (A[k][k].constructor !== (new Complex).constructor) {
+                A[k][k] = new Complex(A[k][k],0);                                
+            }
+            if (A[i][k].constructor !== (new Complex).constructor) {
+                A[i][k] = new Complex(A[i][k],0);                                
+            }
+            F = A[i][k].cdiv(A[k][k]);
+            for (let j = 0; j < n; j++) {
+                if (A[i][j].constructor !== (new Complex).constructor) {
+                    A[i][j] = new Complex(A[i][j],0);                                
+                }
+                if (A[k][j].constructor !== (new Complex).constructor) {
+                    A[k][j] = new Complex(A[k][j],0);                                
+                }
+                A[i][j] = A[i][j].csub(F.cmul(A[k][j]));
+            }
+            if (b[i][0].constructor !== (new Complex).constructor) {
+                b[i][0] = new Complex(b[i][0],0);                                
+            }
+            if (b[k][0].constructor !== (new Complex).constructor) {
+                b[k][0] = new Complex(b[k][0],0);                                
+            }
+            b[i][0] = b[i][0].csub(F.cmul(b[k][0]));
+        }
+    }
+    // Reverse substitution process
+    x = new Array(n); // Array with n elements
+    for (let i = 0; i < n; i++) {
+        x[i] = new Complex(0,0);
+    }
+    for (let i = n-1; i > -1; i--) {
+        let suma = new Complex(0,0);
+        for (let j = i+1; j < n; j++) {
+            suma = suma.cadd(A[i][j].cmul(x[j]));
+        }
+        x[i] = b[i][0].csub(suma).cdiv(A[i][i]);
+    }
+    // Transpose result 
+    //x = x';
+    x = transpose(x);
+    return x;
+}
+
 let A = [[1,-1],[2,0]];
 let b = [[5],[2]];
 console.log(elimGauss(A,b));
@@ -51,5 +136,5 @@ console.log(elimGauss(A,b));
 let a1 = new Complex(1,-4);
 A = [[a1,-1],[2,0]];
 b = [[5],[2]];
-console.log(elimGauss(A,b));
+console.log(elimGaussComplex(A,b));
 
--- README.md
+++ README.md
@@ -35,6 +35,23 @@ purposes.
     - Jacobi Method
     - Adams-Bashforth of order IV
     - Adams-Moulton of order II
+    - **Check all the algorithms for complex number support**
+
+- Javascript ***TODO***
+    - Bisection Method
+    - Gaussian Elimination Method with pivot modification
+    - False Position Method
+    - Runge-Kutta Method of order II and IV
+    - Newton Raphson Method (+ a modification)
+    - Secant Method
+    - Hermite Polynomial
+    - Steffensen's Method
+    - Euler Method
+    - Lagrange Polynomial
+    - Jacobi Method
+    - Gauss-Seidel Method
+    - Adams-Bashforth of order IV
+    - Adams-Moulton of order II
 
 ## Languages TODO
 - Julia
